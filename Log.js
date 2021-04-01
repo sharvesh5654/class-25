f360479c5ddff24397a9086cf27e879850cf9bb3
sharvesh5654 <78212109+sharvesh5654@users.noreply.github.com>
2021-04-01 17:58:11 +0400

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,12 @@
+class Log extends BaseClass {
+  constructor(x, y,height,angle) {
+    
+    super(x, y, 15, height, angle );
+ 
+    this.image = loadImage("sprites/wood2.png");
+
+    Matter.Body.setAngle(this.body,angle)
+   }
+  
+ };
+  
